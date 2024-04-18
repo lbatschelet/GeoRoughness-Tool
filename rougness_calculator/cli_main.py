e5567763e725d5a15654c13a46564063ed1218eb
Lukas Batschelet (he/him) <lukas.batschelet@students.unibe.ch>
2024-04-18 13:56:50 +0200

    Added documentation to cli_main.py

--- rougness_calculator/cli_main.py
+++ rougness_calculator/cli_main.py
@@ -1,14 +1,33 @@
-from application_driver import ApplicationDriver
+"""
+cli_main.py
+-----------
+Version: 0.1.0
+Author: Lukas Batschelet
+Date: 18.04.2024
+-----------
+FOR TESTING PURPOSES ONLY
 
-# TODO: Write documentation
+This module acts as the testing entry point for the application.
+It is responsible for running the application in a command-line interface (CLI) environment.
+It initializes the ApplicationDriver with the input and output paths and runs the application.
+The final version of the application is intended to be run using the GUI provided in gui_main.py.
+"""
+from application_driver import ApplicationDriver
 
 
 def main():
+    """
+    Main function for running the application in a CLI environment.
+    The input and output paths are hardcoded in this script for testing purposes.
+    """
+
     input_path = '/Users/lukasbatschelet/Library/CloudStorage/OneDrive-UniversitaetBern/Studium/FS24/Geoprocessing-II/Projektarbeit_Fotogrammetrie/20240415_Grundlagen/202311xx_Rohdaten/20231116_DEM_Sammler_Obermad_0.05m.tif'
     output_path = '/Users/lukasbatschelet/Library/CloudStorage/OneDrive-UniversitaetBern/Studium/FS24/Geoprocessing-II/Projektarbeit_Fotogrammetrie/05_Output/20231116_DEM_Sammler_Obermad_0.05m.tif'
+
     driver = ApplicationDriver(input_path, output_path)
     driver.run()
 
 
+# Run the application in a CLI environment
 if __name__ == '__main__':
     main()
