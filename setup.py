28cb2360fa7bd10554de132025e743037d996fd8
Lukas Batschelet (he/him) <lukas.batschelet@students.unibe.ch>
2024-04-21 18:34:34 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='dem-roughness-calculator',
-    version='1.1.0',
-    packages=['roughness_calculator', 'roughness_calculator.tests', 'roughness_calculator.classes'],
+    version='1.0.0',
+    packages=['roughness_calculator', 'tests', 'roughness_calculator.classes'],
     url='',
     license='',
     author='lukasbatschelet',
